0af796d4cf61c0650d0e1ca2b10099a06c645364
a.berezhniuk <a.berezhniuk@booongo.com>
2020-10-04 16:29:52 +0300

    Copied from habr (without VM)

---
+++ main.py
@@ -0,0 +1,9 @@
+from parcer import Parser
+from lexer import Lexer
+
+if __name__ == '__main__':
+    file = open('input.txt', 'r')
+    L = Lexer(file)
+    P = Parser(L)
+    n = P.parse()
+    print(n)
