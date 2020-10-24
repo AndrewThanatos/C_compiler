c954069aa520de908e94ed166f5dab44c0d1d513
a.berezhniuk <a.berezhniuk@booongo.com>
2020-10-24 18:29:46 +0300

    READY FOR EXE

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from parcer import Parser
 from lexer import Lexer
 from compiler import Compiler, VirtualMachine
-from utils import print_block
 import sys
 
 
@@ -18,7 +17,6 @@ if __name__ == '__main__':
     V = VirtualMachine()
     n = P.parse()
     comp = C.compile(n)
-    print_block(n)
     V.run(comp)
 
 
